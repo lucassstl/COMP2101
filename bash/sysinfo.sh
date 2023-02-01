402e0a7c8293e40633daa819c6018cce24637713
lucas <lucassstl@gmail.com>
2023-02-01 18:38:34 -0500

    2023-02-01

--- bash/sysinfo.sh
+++ bash/sysinfo.sh
@@ -1,2 +1,26 @@
 #!/bin/bash
-echo $HOSTNAME
+#this is runing a srcip that is going to make variables that have information on the OS ->
+#avalable for the script
+source /etc/os-release
+
+
+#this part of the script is going to colect the information the is going to be->
+#displayed
+var1=$(hostname -A)
+var2=$(hostname -I)
+var3=$(df -h /root --output=avail | tail -n +2)
+
+
+#this is going to print the folowing information 
+#(the fuly qualifyde domain name of the host)
+#(the operating system and the curent version of it)
+#(the IP address of the host machine)
+#(the amont of disk space avalable on the Root filesystem )
+echo "
+===========================================
+FQDN:                    $var1
+OS:                      $PRETTY_NAME
+ip:                      $var2
+disk space in root:     $var3
+===========================================
+"
